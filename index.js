aee4dc45136a3e459395d5d04b79c52e9c25cfea
Kayvan Vakili <kvakili813@gmail.com>
2018-08-31 19:03:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,100 +1,108 @@
-let store = {customers: [], meals: [], deliveries: [], employers: []}
-let customerId = 0;
+let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0;
 let mealId = 0;
+let customerId = 0;
 let deliveryId = 0;
-let employerId = 0;
 
-class Customer {
-  constructor(name, employer) {
+class Neighborhood {
+  constructor(name){
     this.name = name;
-    this.employer = employer;
-    this.id = ++customerId;
-    store.customers.push(this)
+    this.id = neighborhoodId++;
+    store.neighborhoods.push(this);
   }
 
-  meals() {
-    let arr = this.deliveries().map(delivery => delivery.mealId)
-    return(arr.map(custMeal => store.meals.find(meal => meal.id === custMeal)))
-  }
 
   deliveries() {
-    return(store.deliveries.filter(delivery => delivery.customerId === this.id))
+    return store.deliveries.filter(delivery => delivery.neighborhoodId === this.id);
   }
 
-  totalSpent() {
-    return(this.meals().reduce(function(sum, value) {
-      return (sum + value.price)
-    }, 0))
+
+  customers(){
+    return store.customers.filter(customer => customer.neighborhoodId === this.id);
+  }
+
+
+  meals(){
+    let meals = this.deliveries().map(delivery => delivery.meal());
+    return meals.filter(function(meal, index, meals) {
+      return meals.indexOf(meal) === index;
+    });
   }
 }
 
+class Customer {
+
+  constructor(name, neighborhoodId){
+    this.name = name;
+    this.neighborhoodId = neighborhoodId;
+    this.id = ++customerId;
+    store.customers.push(this);
+  }
+
+  deliveries(){
+    return store.deliveries.filter(delivery => delivery.customerId === this.id);
+  }
+
+
+  meals(){
+    return this.deliveries().map(delivery => delivery.meal())
+  }
+
+
+  totalSpent(){
+    return this.meals().reduce(function (accumulator, meal){
+      return accumulator + meal.price;
+    }, 0);
+  }
+}
+
+
 class Meal {
-  constructor(title, price) {
+  constructor(title, price){
     this.title = title;
     this.price = price;
-    this.id = ++mealId
-    store.meals.push(this)
+    this.id = mealId++;
+    store.meals.push(this);
   }
 
-  deliveries() {
-    return(store.deliveries.filter(delivery => delivery.mealId === this.id))
+
+  deliveries(){
+    return store.deliveries.filter(delivery => delivery.mealId === this.id);
   }
 
-  customers() {
-    let arr = this.deliveries().map(delivery => delivery.customerId)
-    return(arr.map(cust => store.customers.find(customer => customer.id === cust)))
+  customers(){
+    return this.deliveries().map(delivery => delivery.customer())
   }
 
   static byPrice() {
-    return(store.meals.sort((a, b) => b.price - a.price))
+    return store.meals.sort(function (meal1, meal2){
+      return meal2.price > meal1.price
+    });
   }
+
 }
+
 
 class Delivery {
-  constructor(meal, customer) {
-    !!meal ? this.mealId = meal.id : null;
-    !!customer ? this.customerId = customer.id : null;
+  constructor(mealId, neighborhoodId, customerId){
+    this.mealId = mealId;
+    this.neighborhoodId = neighborhoodId;
+    this.customerId = customerId;
     this.id = ++deliveryId;
-    store.deliveries.push(this)
+    store.deliveries.push(this);
+  };
+
+  meal(){
+    return store.meals.find(meal => meal.id === this.mealId);
   }
 
-  customer() {
-    return(store.customers.find(customer => customer.id === this.customerId))
+  customer(){
+    return store.customers.find(customer => customer.id === this.customerId);
   }
 
-  meal() {
-    return(store.meals.find(meal => meal.id === this.mealId))
-  }
-}
-
-class Employer {
-  constructor(name) {
-    this.name = name;
-    this.id = ++employerId
-    store.employers.push(this)
+  neighborhood(){
+    return store.neighborhoods.find(neighborhood => neighborhood.id === this.neighborhoodId);
   }
 
-  employees() {
-    return(store.customers.filter(customer => customer.employer.id === this.id))
-  }
-
-   deliveries() {
-    let arr = [];
-    this.employees().map(employee => arr.push(employee.deliveries()))
-    return(arr.reduce((a, b) => a.concat(b), []))
-   }
-
-   meals() {
-     let arr = this.deliveries().map(delivery => delivery.meal());
-     return([...new Set(arr)])
-   }
-
-   mealTotals() {
-     let arr = this.deliveries().map(delivery => delivery.meal());
-     let mealCount = {};
-     arr.forEach(function(meal) {
-       mealCount[meal.id] = (mealCount[meal.id]||0) + 1;
-     });
-    return(mealCount);
-    }
 }
